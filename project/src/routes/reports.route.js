18002aa371f8bd8d0eda063f2b4ad70afef5a590
Jamyle Elen <Jamyle Elen>
2024-08-12 20:50:43 -0300

    remove: rename

--- project/src/routes/reports.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { reports } from "../controllers/reportsController.js";
-
-const reportsRouter = Router();
-
-reportsRouter.get("/reports/:companyId", reports);
-
-export { reportsRouter };
