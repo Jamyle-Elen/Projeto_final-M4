c5603ef6d5843e880b671104436bf2ad1e2a5a23
Jamyle Elen <Jamyle Elen>
2024-08-12 20:49:33 -0300

    feat: added cors, default route maintained

--- project/src/server.js
+++ project/src/server.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import { wastesRouter } from './routes/wastes.route.js';
-import { reportsRouter } from './routes/reports.route.js';
+import routes from './routes/routes.js';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
-app.use(wastesRouter);
-app.use(reportsRouter);
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
