7922e33a013692889b81de602f4859e01e626f41
sauloteles <saulot3l35@gmail.com>
2024-08-14 20:14:10 -0300

    new router

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ import { companyRouter } from './company.routes.js';
 import { collectionRouter } from './collectionRequest.routes.js'; 
 import { checkRouter } from './healthCheck.routes.js';
 import authRouter from './auth.routes.js';
+import { hello } from './hello.routes.js';
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.use('/', reportsRouter);
 router.use('/', companyRouter);
 router.use('/', collectionRouter);
 router.use('/', checkRouter);
+router.use('/', hello);
 
 export default router;
