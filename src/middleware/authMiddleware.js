f4798d8f660ac80e0f1969ba4e5b4c8b8b5de329
Jamyle Elen <Jamyle Elen>
2024-08-14 01:16:50 -0300

    feat: authenticateToken

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+export const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) return res.status(401).json({ message: 'No token provided' });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Forbidden' });
+        req.user = user;
+        next();
+    });
+};
